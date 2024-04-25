7642c7182c5b444fbdd85ab4758b799a97e95253
Nicollaschlemm <119978184+Nicollaschlemm@users.noreply.github.com>
2024-04-24 21:36:31 -0300

    Add files via upload

---
+++ Atividade11/Script.js
@@ -0,0 +1,24 @@
+var funcionario1 = new Object();
+funcionario1.Nome = "Clebson";
+funcionario1.Matricula = "4002";
+funcionario1.Funcao = "Arqueologo"
+alert(`Matricula: ${funcionario1.Matricula}\n
+ Nome: ${funcionario1.Nome}\n
+ Função: ${funcionario1.Funcao}`);
+
+ var funcionario1 = {}
+ funcionario1.Nome = "Robson";
+ funcionario1.Matricula = "8922";
+ funcionario1.Funcao = "Arquiteto";
+ alert(`Matricula: ${funcionario1.Matricula}\n
+ Nome: ${funcionario1.Nome}\n
+ Função: ${funcionario1.Funcao}`);
+
+ var funcionario1 = {
+    Matricula: "1234",
+    Nome: "Piton",
+    Funcao: "Filosofo"
+ };
+ alert(`Matricula: ${funcionario1.Matricula}\n
+ Nome: ${funcionario1.Nome}\n
+ Função: ${funcionario1.Funcao}`);
